d4b68453192e07022e3100d674edd8f321843f95
jroshansafi <jroshansafi@gmail.com>
2021-06-06 17:39:37 +0530

    i did if else exercise

---
+++ 1_variables/constant/if_else.js
@@ -0,0 +1,27 @@
+/* "if" and "else" and "elseif"
+if (condition) {
+    statement
+}
+elseif (condition) {
+    statement
+}
+elseif (condition) 
+    statement
+else {
+    statement
+}
+*/
+
+let hour = 17;
+if (hour >=6 && hour < 12) {
+    console.log("Good Morning");
+}
+else if (hour >=12 && hour < 18) {
+    console.log("Good Afternoon");
+}    
+else {
+    console.log("Good Evening");
+}    
+
+
+
