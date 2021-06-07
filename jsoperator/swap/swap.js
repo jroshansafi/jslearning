72f6ffd4960bcf40f515e9ba057e9a317236bf6f
jroshansafi <jroshansafi@gmail.com>
2021-06-08 00:17:09 +0530

    i added precedence

--- jsoperator/swap/swap.js
+++ jsoperator/swap/swap.js
@@ -1,10 +1,14 @@
-let a = 'red';
-let b = 'blue';
+// let a = 'red';
+// let b = 'blue';
 
-let c = a;
-a=b;
- b = c;
+// let c = a;
+// a=b;
+//  b = c;
 
 
-console.log(a);
-console.log(b);
+// console.log(a);
+// console.log(b);
+
+//precedence
+ let a= (2 + 3 )* 4;
+ console.log(a);
